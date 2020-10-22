e41910d723982bcd7eab08d1252eb0e35c97efd4
kodle <21694620+kodle@users.noreply.github.com>
2020-10-22 13:32:47 +0200

    Update script.js

--- mc/script.js
+++ mc/script.js
@@ -1,7 +1,7 @@
 //based on a pen by @robinselmer
-var url = "https://api.minetools.eu/ping/compagnie.minesr.com";
+var url = "https://api.minetools.eu/ping/mc.kodle.xyz";
 
-//https://mcapi.us/server/status?ip=GAME-FR-60.MTXSERV.COM&port=27450
+//https://mcapi.us/server/status?ip=
 
 $.getJSON(url, function(r) {
     //data is the JSON string
@@ -11,7 +11,7 @@ $.getJSON(url, function(r) {
  }
 var pl = '';
  if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  }
-  $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Joueurs en ligne:</b> '+r.players.online+"</br><b>IP:</b> compagnie.minesr.com"+"</br>"+"<a href=\"https://discord.gg/BERvkAF\"><i class=\"fab fa-discord\"></i></a>");
+  $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Joueurs en ligne:</b> '+r.players.online+"</br><b>IP:</b> mc.kodle.xyz"+"</br>"+"<a href=\"https://discord.gg/BERvkAF\"><i class=\"fab fa-discord\"></i></a>");
  $('#favicon').attr('src', r.favicon);
 
 });
