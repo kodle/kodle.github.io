d315efac15c7cd8929ee7ba56780dc494c9f377d
kodle <21694620+kodle@users.noreply.github.com>
2020-10-23 10:48:35 +0200

    Update script.js

--- mc/script.js
+++ mc/script.js
@@ -11,7 +11,9 @@ $.getJSON(url, function(r) {
  }
 var pl = '';
  if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  }
-  $('#rest').html('<br><b>Joueurs en ligne:</b> '+r.players.online+"</br><b>IP:</b> mc.kodle.xyz"+"</br>"+"<a href=\"https://discord.gg/BERvkAF\"><i class=\"fab fa-discord\"></i></a>");
+  $('#rest').html('<br><b>Joueurs en ligne:</b> '+r.players.online+"</br><b>IP:</b> mc.kodle.xyz"+"</br>"+"<a href=\"https://discord.gg/BERvkAF\"><i class=\"fab fa-discord\"></i></a><a href=\"http://www.kodle.xyz/don\"><i class=\"fab fa-paypal\"></i></a>");
  $('#favicon').attr('src', r.favicon);
 
 });
+
+// r.description.replace(/§(.+?)/gi, '')+
